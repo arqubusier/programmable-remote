0eb1f02329ca20ca1cf6bc51eca7ae018740e758
Herman Lundkvist <herman.lundkvist@gmail.com>
2020-05-01 17:46:16 +0200

    approximate 38kHz pulse with TIM2.

--- src/remote.cxx
+++ src/remote.cxx
@@ -12,6 +12,8 @@ namespace util {
 struct timer_t {
   uint32_t timer;
   tim_oc_id channel;
+  uint32_t auto_reload_period;
+  uint32_t prescaler;
 };
 struct io_t{
   uint32_t port;
@@ -21,9 +23,11 @@ struct io_t{
 
 #define LED1_PORT GPIOC
 #define LED1_PIN GPIO13
-#define IR_SENSOR_PORT GPIOA
-#define IR_SENSOR_PIN GPIO1
-constexpr const util::timer_t output_ir_timer{TIM2, TIM_OC1};
+constexpr const util::timer_t output_ir_timer{TIM2, TIM_OC1, 65535, 5000};
+// Do not divide clock for highest possible resolution
+// Frequency is ABP1 clock * 2 = 72 MHz.
+// 1 period = 947 * (1/72MHz) = 13.15277... us
+constexpr const util::timer_t carrier_timer{TIM2, TIM_OC1, 474, 1};
 constexpr const util::io_t output_ir{GPIOA,GPIO0};
 constexpr const util::io_t input_ir{GPIOA,GPIO1};
 /* Morse standard timings */
@@ -76,6 +80,9 @@ static void gpio_setup(void)
   rcc_periph_clock_enable(RCC_GPIOA);
   // Enable AFIO clock.
   rcc_periph_clock_enable(RCC_AFIO);
+
+  // carrier timer output compare value on pin, connect to ir led.
+  gpio_set_mode(output_ir.port, GPIO_MODE_OUTPUT_50_MHZ, GPIO_CNF_OUTPUT_ALTFN_PUSHPULL, output_ir.pin);
   /*
   gpio_set_mode(input_ir.port, GPIO_MODE_INPUT, GPIO_CNF_INPUT_FLOAT, input_ir.pin);
   nvic_enable_irq(NVIC_EXTI1_IRQ);
@@ -86,7 +93,37 @@ static void gpio_setup(void)
   */
 }
 
-static void tim_setup(void)
+static void carrier_tim_setup(void)
+{
+	// Enable carrier_timer clock.
+	rcc_periph_clock_enable(RCC_TIM2);
+	// Reset TIM2 peripheral to defaults.
+	rcc_periph_reset_pulse(RST_TIM2);
+
+	// Timer global mode:
+	// - No divider
+	// - Up-couning (Alignment edge, Direction up)
+	timer_set_mode(carrier_timer.timer, TIM_CR1_CKD_CK_INT,
+		TIM_CR1_CMS_EDGE, TIM_CR1_DIR_UP);
+
+	timer_set_prescaler(carrier_timer.timer, carrier_timer.prescaler);
+	timer_continuous_mode(carrier_timer.timer);
+
+	// count full range, as we'll update compare value continuously.
+	timer_set_period(carrier_timer.timer, carrier_timer.auto_reload_period);
+
+	// Set the initual output compare value for OC1.
+	timer_set_oc_value(carrier_timer.timer, carrier_timer.channel, carrier_timer.auto_reload_period);
+        timer_set_oc_mode(carrier_timer.timer, carrier_timer.channel, TIM_OCM_TOGGLE);
+        timer_enable_oc_output(carrier_timer.timer, carrier_timer.channel);
+
+	// Counter enable.
+	timer_enable_counter(carrier_timer.timer);
+
+        // interrupts and DMA requests are disabled by default.
+}
+
+static void command_tim_setup(void)
 {
 	// Enable output_ir_timer clock.
 	rcc_periph_clock_enable(RCC_TIM2);
@@ -178,7 +215,7 @@ void exti1_isr(void)
 int main(void) {
   clock_setup();
   gpio_setup();
-  tim_setup();
+  carrier_tim_setup();
   while(true) {
   }
 }
