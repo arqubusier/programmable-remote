c1dfb2eade4424aea54b94873f6b8410f1137ab7
Herman Lundkvist <herman.lundkvist@gmail.com>
2020-05-02 16:24:33 +0200

    Input interrupt.

--- src/remote.cxx
+++ src/remote.cxx
@@ -17,15 +17,15 @@ struct timer_t {
   uint32_t auto_reload_period;
   uint32_t prescaler;
 };
+
 struct io_t{
   uint32_t port;
   uint32_t pin;
 };
 }
 
-#define LED1_PORT GPIOC
-#define LED1_PIN GPIO13
-constexpr const util::timer_t output_ir_timer{TIM2, RCC_TIM2, RST_TIM2, TIM_OC1, 65535, 5000};
+
+constexpr const util::timer_t command_timer{TIM3, RCC_TIM3, RST_TIM3, TIM_OC1, 65535, 360};
 // Do not divide clock for highest possible resolution
 // Frequency is ABP1 clock * 2 = 36 MHz.
 // 1 period = 947 * (1/72MHz) = 13.15277... us
@@ -40,6 +40,7 @@ constexpr const util::io_t output_ir{GPIOA,GPIO0}; // TIM2 CH1 output
 //constexpr const util::io_t output_ir{GPIOA,GPIO6}; // TIM3 CH1 output
 
 constexpr const util::io_t input_ir{GPIOA,GPIO1};
+constexpr const util::io_t led_ir{GPIOC,GPIO13};
 
 static void clock_setup(void)
 {
@@ -47,34 +48,37 @@ static void clock_setup(void)
 
   // Enable GPIO clock for leds.
   rcc_periph_clock_enable(RCC_GPIOC);
+
+  // Enable GPIO clock for IR input.
   rcc_periph_clock_enable(RCC_GPIOA);
 
   // Enable AFIO clock for timers.
   rcc_periph_clock_enable(RCC_AFIO);
-  // Enable carrier_timer clock.
+
+  // Enable timer clocks.
   rcc_periph_clock_enable(carrier_timer.rcc_tim);
   rcc_periph_reset_pulse(carrier_timer.rst_tim);
+  rcc_periph_clock_enable(command_timer.rcc_tim);
+  rcc_periph_reset_pulse(command_timer.rst_tim);
 
 }
 
 static void gpio_setup(void)
 {
-
   // Enable led as output
-  gpio_set_mode(LED1_PORT, GPIO_MODE_OUTPUT_50_MHZ,
-            GPIO_CNF_OUTPUT_PUSHPULL, LED1_PIN);
-  gpio_set(LED1_PORT, LED1_PIN);
+  gpio_set_mode(led_ir.port, GPIO_MODE_OUTPUT_50_MHZ,
+            GPIO_CNF_OUTPUT_PUSHPULL, led_ir.pin);
+  gpio_set(led_ir.port, led_ir.pin);
 
   // carrier timer output compare value on pin, connect to ir led.
   gpio_set_mode(output_ir.port, GPIO_MODE_OUTPUT_50_MHZ, GPIO_CNF_OUTPUT_ALTFN_PUSHPULL, output_ir.pin);
-  /*
+
+  // Ir input interrupt.
   gpio_set_mode(input_ir.port, GPIO_MODE_INPUT, GPIO_CNF_INPUT_FLOAT, input_ir.pin);
   nvic_enable_irq(NVIC_EXTI1_IRQ);
-  // Configure the EXTI subsystem.
   exti_select_source(EXTI1, input_ir.port);
-  exti_set_trigger(EXTI1, EXTI_TRIGGER_FALLING);
+  exti_set_trigger(EXTI1, EXTI_TRIGGER_BOTH);
   exti_enable_request(EXTI1);
-  */
 }
 
 static void carrier_tim_setup(void)
