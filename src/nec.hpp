4dca4baac551cb1513b6f8c3e41dde124c9acbff
Herman Lundkvist <herman.lundkvist@gmail.com>
2020-05-03 22:13:06 +0200

    Changing ir handling WIP.

--- src/nec.hpp
+++ src/nec.hpp
@@ -5,23 +5,27 @@
 
 #include "util.hpp"
 
-const uint32_t tim3_psc = TIM3_PSC;
-
 class NecHandler {
 public:
   enum : uint32_t { START = 0, START_SPACE, DATA_OFFSET };
-  const uint32_t N_DATA_BITS = 32;
+  constexpr static const uint32_t N_DATA_BITS = 32;
+  constexpr static const uint32_t START_NS = 9000000;
+  constexpr static const uint32_t START_SPACE_NS = 4500000;
+  constexpr static const uint32_t CARRIER_PULSE = 560000;
+  constexpr static const uint32_t CARRIER_SPACE_1 = 1690000;
+  constexpr static const uint32_t CARRIER_SPACE_0 = 560000;
   uint32_t state_ = START;
 
   template <typename HandlerImplementationT>
   void handle(HandlerImplementationT &handler_implementation) {
     bool success = false;
+    /*
     if (state_ == START) {
       success = handler_implementation.start();
     } else if (state_ == START_SPACE) {
       success = handler_implementation.start_space();
     } else if (is_data(state_)) {
-      success = handler_implementation.data();
+      success = handler_implementation.data(state);
     } else if (is_data_space(state_)) {
       success = handler_implementation.data_space();
     } else if (is_end(state_)) {
@@ -29,6 +33,9 @@ public:
     } else {
       // should never happend
     }
+    */
+
+    success = handle_sub(state);
 
     if (success) {
       if (is_end(state_)) {
@@ -65,55 +72,91 @@ private:
 
 class InputHandler final : public NecHandler {
   util::timer_t const &timer_;
+  bool
 
-public:
-  InputHandler(util::timer_t const &timer) : timer_{timer} {}
+      public : InputHandler(util::timer_t const &timer)
+      : timer_{timer} {}
+
+  bool handle_sub(uint32_t state) {
+    uint32_t delta = timer_get_counter(timer_.tim);
+    uint32_t threshold = util::ns2count(timer_, 2000000);
+
+    if (state_ == START) {
+      timer_enable_counter(timer_.tim);
+    } else if (state_ == START_SPACE) {
+      uint32_t delta = timer_get_counter(timer_.tim);
+      uint32_t threshold = util::ns2count(timer_, 1800000);
+      success =
+          util::valid_delta(delta, util::ns2count(timer_, START_NS), threshold);
+    } else if (state == START_SPACE + 1) {
+      uint32_t delta = timer_get_counter(timer_.tim);
+      uint32_t threshold = util::ns2count(timer_, 1000000);
+      success = util::valid_delta(delta, util::ns2count(timer_, START_SPACE_NS),
+                                  threshold);
+    } else if (is_data(state_)) {
+      uint32_t delta = timer_get_counter(timer_.tim);
+      uint32_t threshold = util::ns2count(timer_, 950000);
+      success = util::valid_delta(delta, util::ns2count(timer_, START_SPACE_NS),
+                                  threshold);
+    } else if (is_data_space(state_)) {
+      uint32_t delta = timer_get_counter(timer_.tim);
+      uint32_t threshold0 = util::ns2count(timer_, 100000);
+      uint32_t threshold1 = util::ns2count(timer_, 300000);
+      if (success = util::valid_delta(
+              delta, util::ns2count(timer_, CARRIER_SPACE_0_NS), threshold0)) {
+      } else if (success = util::valid_delta(
+                     delta,
+                     util::ns2count(timer_, CARRIER_SPACE_1_NS, threshold1))) {
+      }
+    } else if (is_end(state_)) {
+    } else {
+      // should never happend
+    }
+
+    timer_set_counter(timer_.tim, 0);
+  }
 
   bool start() {
-    bool success = false;
     timer_set_counter(timer_.tim, 0);
     timer_enable_counter(timer_.tim);
-    success = true;
-    return success;
+    return true;
   }
   bool start_space() {
-    bool success = false;
     uint32_t delta = timer_get_counter(timer_.tim);
+    uint32_t threshold = util::ns2count(timer_, 2000000);
+
     timer_set_counter(timer_.tim, 0);
-    return success;
+    return util::valid_delta(delta, util::ns2count(timer_, START_NS),
+                             threshold);
   }
   bool data() {
-    bool success = false;
-    return success;
+    uint32_t delta = timer_get_counter(timer_.tim);
+    uint32_t threshold = util::ns2count(timer_, 1000000);
+
+    timer_set_counter(timer_.tim, 0);
+    return util::valid_delta(delta, util::ns2count(timer_, START_SPACE_NS),
+                             threshold);
   }
   bool data_space() {
-    bool success = false;
-    return success;
+    uint32_t delta = timer_get_counter(timer_.tim);
+    uint32_t threshold = util::ns2count(timer_, 1000000);
+
+    timer_set_counter(timer_.tim, 0);
+    return util::valid_delta(delta, util::ns2count(timer_, START_SPACE_NS),
+                             threshold);
   };
   bool end() {
     bool success = false;
     return success;
   };
   void reset() {
-    // Timer global mode:
-    // - No divider
-    // - Up-couning (Alignment edge, Direction up)
     timer_set_mode(timer_.tim, TIM_CR1_CKD_CK_INT, TIM_CR1_CMS_EDGE,
                    TIM_CR1_DIR_UP);
 
-    timer_set_prescaler(timer_.tim, timer_.input_clock/timer_.frequency);
+    timer_set_prescaler(timer_.tim, timer_.input_clock / timer_.frequency);
     timer_continuous_mode(timer_.tim);
 
     timer_set_period(timer_.tim, timer_.auto_reload_period);
-
-    // timer_set_oc_value(command_timer.tim, command_timer.channel,
-    // command_timer.auto_reload_period); timer_set_oc_mode(command_timer.tim,
-    // command_timer.channel, TIM_OCM_TOGGLE);
-    // timer_enable_oc_output(command_timer.tim, carrier_timer.channel);
-
-    // Counter enable.
-    // timer_enable_counter(carrier_timer.tim);
-    // interrupts and DMA requests are disabled by default.
   };
 };
 
--- src/util.hpp
+++ src/util.hpp
@@ -1,10 +1,11 @@
 #ifndef UTIL_HPP
 #define UTIL_HPP
 
+#include <bits/stdint-uintn.h>
 #include <stdint.h>
 
-#include <libopencm3/stm32/timer.h>
 #include <libopencm3/stm32/rcc.h>
+#include <libopencm3/stm32/timer.h>
 
 namespace util {
 struct timer_t {
@@ -17,10 +18,25 @@ struct timer_t {
   uint32_t frequency;
 };
 
-struct io_t{
+struct io_t {
   uint32_t port;
   uint32_t pin;
 };
+
+constexpr uint32_t ns2count(timer_t const &timer, uint32_t val_us) {
+  return static_cast<uint32_t>(val_us * timer.frequency / 1000000000);
 }
+
+/*
+ * \brief Test wether the given time delta is within acceptable limit.
+ */
+bool valid_delta(uint32_t delta, uint32_t target, uint32_t threshold) {
+  return (delta > target - threshold && delta < target + threshold);
+}
+
+constexpr const uint32_t MEGA = 1000000;
+constexpr const uint32_t KILO = 1000000;
+
+} // namespace util
 
 #endif // UTIL_HPP
